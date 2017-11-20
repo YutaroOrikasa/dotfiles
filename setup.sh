436ba572c004ec0979429f0a1d025afb06873a15
dummy <dummy>
2017-11-20 23:31:26 +0900

    Refactor

--- setup.sh
+++ setup.sh
@@ -4,4 +4,4 @@ set -xv
 
 cd ~
 
-ln -s $(ls -1d dotfiles/.* | grep -v -E '(dotfiles/.|dotfiles/..|dotfiles/.git|dotfiles/.gitignore)$') ./
+ln -s $(ls -1d dotfiles/.* | grep -v -E 'dotfiles/(.|..|.git|.gitignore)$') ./
