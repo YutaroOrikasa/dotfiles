6f09d7fbf802f2d17d0e736497eacaa1879039ee
dummy <dummy>
2017-11-21 10:28:04 +0900

    Make setup.sh be able to replace old dot files

--- setup.sh
+++ setup.sh
@@ -2,6 +2,16 @@
 
 set -xv
 
-cd ~
+cd ~/dotfiles
 
-ln -s $(ls -1d dotfiles/.* | grep -v -E 'dotfiles/(.|..|.git|.gitignore)$') ./
+case "$1" in
+    -r|--replace)
+        mkdir ~/old_dotfiles || exit
+        for file in $(ls -1d .* | grep -v -E '(\.|\.\.|\.git|\.gitignore)$');do
+            mv ~/"$file" ~/old_dotfiles
+        done
+         ;;
+esac
+
+cd ~
+ln -s $(ls -1d dotfiles/.* | grep -v -E 'dotfiles/(\.|\.\.|\.git|\.gitignore)$') ~/
