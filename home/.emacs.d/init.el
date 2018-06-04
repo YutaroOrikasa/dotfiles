70a598d6979d67dc3732e8f7798aae5b30dcd659
‘Yutaro <orikasa@csg.ci.i.u-tokyo.ac.jp>
2018-06-04 17:01:29 +0900

    emacs: Remove key bind S-<cursor move> as move window

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -217,11 +217,6 @@
 (global-set-key (kbd "C-x S-<up>")    'windmove-up)
 (global-set-key (kbd "C-x S-<down>")  'windmove-down)
 
-(global-set-key (kbd "S-<left>")  'windmove-left)
-(global-set-key (kbd "S-<right>") 'windmove-right)
-(global-set-key (kbd "S-<up>")    'windmove-up)
-(global-set-key (kbd "S-<down>")  'windmove-down)
-
 ;; key bindings for change window size
 (global-set-key (kbd "S-M-<left>") 'shrink-window-horizontally)
 (global-set-key (kbd "S-M-<right>") 'enlarge-window-horizontally)
