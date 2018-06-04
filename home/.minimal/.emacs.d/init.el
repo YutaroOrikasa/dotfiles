3cf3ea74cb9a3e2b327f7cfdaeeba04ca9128d86
‘Yutaro <orikasa@csg.ci.i.u-tokyo.ac.jp>
2018-06-04 17:04:33 +0900

    minimal emacs: Add bindings (windmove, toggle truncate)

--- home/.minimal/.emacs.d/init.el
+++ home/.minimal/.emacs.d/init.el
@@ -22,3 +22,29 @@
 (add-hook 'after-save-hook
           'executable-make-buffer-file-executable-if-script-p)
 
+;; key bindings for window moving
+(global-set-key (kbd "C-c <left>")  'windmove-left)
+(global-set-key (kbd "C-c <right>") 'windmove-right)
+(global-set-key (kbd "C-c <up>")    'windmove-up)
+(global-set-key (kbd "C-c <down>")  'windmove-down)
+
+(global-set-key (kbd "C-c C-<left>")  'windmove-left)
+(global-set-key (kbd "C-c C-<right>") 'windmove-right)
+(global-set-key (kbd "C-c C-<up>")    'windmove-up)
+(global-set-key (kbd "C-c C-<down>")  'windmove-down)
+
+(global-set-key (kbd "C-x S-<left>")  'windmove-left)
+(global-set-key (kbd "C-x S-<right>") 'windmove-right)
+(global-set-key (kbd "C-x S-<up>")    'windmove-up)
+(global-set-key (kbd "C-x S-<down>")  'windmove-down)
+
+;; key bindings for change window size
+(global-set-key (kbd "S-M-<left>") 'shrink-window-horizontally)
+(global-set-key (kbd "S-M-<right>") 'enlarge-window-horizontally)
+(global-set-key (kbd "S-M-<down>") 'shrink-window)
+(global-set-key (kbd "S-M-<up>") 'enlarge-window)
+
+
+;; 
+(global-set-key (kbd "C-c t") 'toggle-truncate-lines)
+
