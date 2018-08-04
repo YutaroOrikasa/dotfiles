6b081865b5f9724816f369313357a876a746053e
Yutaro Orikasa <orikasa@csg.ci.i.u-tokyo.ac.jp>
2018-08-04 20:07:56 +0900

    zsh: Remove redefining brew

--- home/.dotfiles-lib/hack/Darwin.sh
+++ home/.dotfiles-lib/hack/Darwin.sh
@@ -1,16 +1,2 @@
 
-function brew {
-    # Change permission of zsh dir for brew's installing on
-    # non privileged mode.
-    sudo chown -R "$USER" /usr/local/share/zsh
-
-    # do brew command
-    command brew "$@"
-
-    # Restore permission of zsh dir for avoiding zsh's error:
-    #     zsh compinit: insecure directories, run compaudit for list.
-    sudo chown -RL root /usr/local/share/zsh
-}
-
-
 alias ls='ls -G'
