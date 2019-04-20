cde8a6e9ded5a93c6299c2379a1b0c77bfc61412
Yutaro Orikasa <yutaro.orikasa.625@gmail.com>
2019-04-20 17:38:45 +0900

    Add hook for git command on osx

--- home/.dotfiles-lib/hack/Darwin.sh
+++ home/.dotfiles-lib/hack/Darwin.sh
@@ -4,3 +4,18 @@ alias ls='ls -G'
 alias compinit='compinit -u'
 
 export PATH=~/.usr/mac/bin:"$PATH"
+
+git () {
+    if [[ "$1" == push ]];then
+        if command git config --list | grep credential.helper=osxkeychain >/dev/null;then
+            (
+                echo 'WARNING'
+                echo 'credential.helper=osxkeychain is set.'
+                echo 'Your credential helper will not work.'
+                echo 'You have to unsed OSX default credential.helper.'
+                command git config --show-origin --list | grep credential.helper
+            ) >&2
+        fi
+    fi
+    command git "$@"
+}
