cfc79b882700e5d4aca027797dbbd3cf78467360
Yutaro Orikasa <yutaro.orikasa.625@gmail.com>
2020-05-20 00:45:32 +0900

    Remove darwin git hook
    I found a method to prevent to use osxkeychain.
    See man gitcredentials CONFIGURATION OPTIONS section
    or https://git-scm.com/docs/gitcredentials#Documentation/gitcredentials.txt-helper.

--- home/.dotfiles-lib/hack/Darwin.sh
+++ home/.dotfiles-lib/hack/Darwin.sh
@@ -6,18 +6,3 @@ alias t='open -a Terminal .'
 alias compinit='compinit -u'
 
 export PATH=~/.usr/mac/bin:"$PATH"
-
-git () {
-    if [[ "$1" == push ]];then
-        if command git config --list | grep credential.helper=osxkeychain >/dev/null;then
-            (
-                echo 'WARNING'
-                echo 'credential.helper=osxkeychain is set.'
-                echo 'Your credential helper will not work.'
-                echo 'You have to unsed OSX default credential.helper.'
-                command git config --show-origin --list | grep credential.helper
-            ) >&2
-        fi
-    fi
-    command git "$@"
-}
