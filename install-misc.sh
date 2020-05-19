38a17f6ff199b782fa4857d776ba10974d64e9df
Yutaro Orikasa <yutaro.orikasa.625@gmail.com>
2020-05-19 21:36:23 +0900

    Move spectacle setting to out of homeshick managed area
    It is not suitable for managed in homeshick because
    Library/Application Support/Spectacle/Shortcuts.json
    is automatically changed on launch.

--- install-misc.sh
+++ install-misc.sh
@@ -2,6 +2,12 @@
 
 case $(uname) in
     Darwin)
+
+        # DefaultKeyBinding
         mkdir -p ~/Library/KeyBindings
         cp -ai ~/.homesick/repos/dotfiles/mac/DefaultKeyBinding.dict ~/Library/KeyBindings/
+
+        # Spectacle
+        mkdir -p ~/Library/Application\ Support/Spectacle/
+        cp -ai ~/.homesick/repos/dotfiles/mac/Spectacle/Shortcuts.json ~/Library/Application\ Support/Spectacle/
 esac
