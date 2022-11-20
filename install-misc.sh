8d511ca21868e8df9db075562e5ddb0b1d9729d5
Yutaro Orikasa <yutaro.orikasa.625@gmail.com>
2022-11-21 08:30:29 +0900

    Manage DefaultKeyBinding.dict by homesick link

--- install-misc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-case $(uname) in
-    Darwin)
-
-        # DefaultKeyBinding
-        mkdir -p ~/Library/KeyBindings
-        ln -sf ~/.homesick/repos/dotfiles/mac/DefaultKeyBinding.dict ~/Library/KeyBindings/
-esac
