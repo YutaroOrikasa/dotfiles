076b8d8ab7a32e3bdc0a2a4a6c46f2317c738953
Yutaro Orikasa <yutaro.orikasa.625@gmail.com>
2023-01-23 23:45:29 +0000

    [git][wsl] Use wslpath in git wrapper

--- home/.dotfiles-lib/hack/os_type/wsl.sh
+++ home/.dotfiles-lib/hack/os_type/wsl.sh
@@ -4,17 +4,13 @@ export LANG=en_US.UTF-8
 
 export PATH="$HOME/.usr/wsl/bin:$PATH"
 
-if ! command -v realpath >/dev/null 2>&1; then
-    echo 'Warning: dotfiles: Please install realpath for git to handle file mode correctly.' >&2
-else
-    git() {
-        if [[ "$(realpath "$PWD")" == /mnt/* ]]; then
+git() {
+        if [[ "$(wslpath -m "$PWD")" =~ [A-Z]:/.*|//.* ]]; then
             command git -c core.filemode=false "$@"
         else
             command git "$@"
         fi
     }
-fi
 
 unalias open
 
