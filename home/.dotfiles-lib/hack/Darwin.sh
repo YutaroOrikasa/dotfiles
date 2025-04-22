0fd3dd8756a8d9a2e2ff9652cfea00529ef0e2c9
Yutaro Orikasa <yutaro.orikasa.625@gmail.com>
2025-04-22 17:28:53 +0900

    [mac] Copy touch id on sudo script to local
    It becomes secure.
    `curl ... | sh` is not secure.

--- home/.dotfiles-lib/hack/Darwin.sh
+++ home/.dotfiles-lib/hack/Darwin.sh
@@ -1,4 +1,3 @@
-
 alias ls='ls -G'
 
 alias t='open -a Terminal .'
@@ -10,4 +9,19 @@ export PATH="/usr/local/sbin:$PATH"
 
 export PATH=~/.usr/mac/bin:"$PATH"
 
-curl -fsSL https://gist.githubusercontent.com/YutaroOrikasa/de463d655e15eac1d959a3f57e988f7e/raw/enable_touch_id_on_sudo.sh | sh
+# Enable touch id on sudo
+# https://gist.githubusercontent.com/YutaroOrikasa/de463d655e15eac1d959a3f57e988f7e
+(
+    grep -q 'auth       sufficient     pam_tid.so' /etc/pam.d/sudo && exit 0
+
+    sudo patch -d/ -b -p0 <<EOF
+--- /etc/pam.d/sudo.orig
++++ /etc/pam.d/sudo
+@@ -1,4 +1,5 @@
+ # sudo: auth account password session
++auth       sufficient     pam_tid.so
+ auth       sufficient     pam_smartcard.so
+ auth       required       pam_opendirectory.so
+ account    required       pam_permit.so
+EOF
+)
